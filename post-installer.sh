bbab20d3a7358596c66d6283c0a076addc72bea0
Aaron Maynard <34851708+aaronmaynard@users.noreply.github.com>
2023-01-06 21:44:51 -0600

    Update post-installer.sh

--- post-installer.sh
+++ post-installer.sh
@@ -8,7 +8,7 @@ echo [POST-INSTALLER] - $(date +"%T") - Starting post-installer script...
 echo [POST-INSTALLER] - $(date +"%T") - Setting up ROS repositories...
 sudo apt install software-properties-common
 sudo add-apt-repository universe
-sudo apt update && sudo apt install curl
+sudo apt update && sudo apt install curl -y
 sudo curl -k -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
